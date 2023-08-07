0d49364c5bbf956dea9bb3ffa1321eb4ba3ff7b5
wenhua0902 <s1100396@gm.pu.edu.tw>
2023-08-07 11:14:29 +0800

    version 2:多刪除記事、更改文字大小及把字體置中

--- app/src/main/java/tw/edu/pu/gm/s1100396/myapplication/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1100396/myapplication/MainActivity.kt
@@ -1,5 +1,4 @@
 package tw.edu.pu.gm.s1100396.myapplication
-
 import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -8,38 +7,34 @@ import android.widget.EditText
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
-
-//第一版:只有新增記事，點擊記錄可以做編輯
-
+//第二版:多刪除記事(當使用者長按某個記事項目時，將彈出一個對話框，確認是否要刪除該記事)、文字大小、置中
 class MainActivity : AppCompatActivity() {
-
     private lateinit var listViewNotes: ListView
     private lateinit var btnAddNote: Button
     private val notesList = mutableListOf<String>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         listViewNotes = findViewById(R.id.listViewNotes)
         btnAddNote = findViewById(R.id.btnAddNote)
-
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, notesList)
         listViewNotes.adapter = adapter
-
         btnAddNote.setOnClickListener {
             showAddNoteDialog()
         }
-
         //設置 ListView 的項目點擊監聽器 (OnItemClickListener)
-        //當用戶點擊 ListView 中的某個項目時，這個監聽器會觸發。
+        //當點擊 ListView 中的某個項目時，這個監聽器會觸發。
         listViewNotes.setOnItemClickListener { _, _, position, _ ->
             //showEditNoteDialog(position)是一個自定義的函式，用於顯示編輯記事的對話框
-            //當用戶點擊 ListView 中的項目時，這個方法將被調用並顯示一個對話框，用戶可以在這個對話框中編輯被點擊的項目的內容
+            //當點擊ListView 中的項目時，這個方法將被調用並顯示一個對話框，使用者可以在這個對話框中編輯被點擊的項目的內容
             showEditNoteDialog(position)
         }
-    }
 
+        listViewNotes.setOnItemLongClickListener { _, _, position, _ ->
+            showDeleteNoteDialog(position)
+            true
+        }
+    }
     //新增記事
     private fun showAddNoteDialog() {
         val editText = EditText(this)
@@ -60,7 +55,6 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButton("取消", null)
             .show()
     }
-
     //編輯記事
     private fun showEditNoteDialog(position: Int) {
         val editText = EditText(this)
@@ -77,6 +71,25 @@ class MainActivity : AppCompatActivity() {
                     notesList[position] = updatedNote
                     (listViewNotes.adapter as ArrayAdapter<String>).notifyDataSetChanged()
                 }
+            }
+            .setNegativeButton("取消", null)
+            .show()
+    }
+
+
+    //刪除記事
+
+    //當使用者長按某個記事項目時，將彈出一個對話框，確認是否要刪除該記事
+    private fun showDeleteNoteDialog(position: Int) {
+        AlertDialog.Builder(this)
+            //設置對話框的標題為 "刪除記事"
+            .setTitle("刪除記事")
+            .setMessage("確定要刪除這個記事嗎？")
+
+            //設置對話框的 "確定" 按鈕，並設置點擊確定按鈕時的行為
+            .setPositiveButton("確定") { _, _ ->
+                notesList.removeAt(position)
+                (listViewNotes.adapter as ArrayAdapter<String>).notifyDataSetChanged()
             }
             .setNegativeButton("取消", null)
             .show()
